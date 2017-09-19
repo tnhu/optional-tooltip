0e4e17c649de59e6e8ed67502fed16420a82951c
Tan Nhu <tannhu@gmail.com>
2017-09-18 21:33:14 -0700

    Bump to 1.2.4

--- OptionalTooltip.js
+++ OptionalTooltip.js
@@ -1,83 +1,94 @@
-try {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }
-
-  var OptionalTooltip = (function(parent) {
-    var upgrade =
-      typeof Reflect === "object"
-        ? function() {
-            return Reflect.construct(parent, arguments, this.constructor);
-          }
-        : function() {
-            return parent.apply(this, arguments) || this;
-          };
-
-    function constructor() {}
-
-    function OptionalTooltip() {
-      var self = upgrade.apply(this, arguments);
-      return constructor.apply(self, arguments) || self;
+function init() {
+  try {
+    function defineProperties(target, props) {
+      for (var i = 0, len = props.length; i < len; i++) {
+        var descriptor = props[i]
+        descriptor.enumerable = descriptor.enumerable || false
+        descriptor.configurable = true
+        if ('value' in descriptor) descriptor.writable = true
+        Object.defineProperty(target, descriptor.key, descriptor)
+      }
     }
 
-    OptionalTooltip.prototype = Object.create(parent.prototype, {
-      constructor: {
-        configurable: true,
-        writable: true,
-        value: OptionalTooltip
-      }
-    });
-
-    OptionalTooltip.prototype.attributeChangedCallback = function attributeChangedCallback(
-      attributeName,
-      oldValue,
-      newValue,
-      namespace
-    ) {
-      var th = this;
-
-      setTimeout(function() {
-        th.adjustTooltip();
-      }, 250);
-    };
-
-    OptionalTooltip.prototype.adjustTooltip = function adjustTooltip() {
-      var ATTR = "data-tooltip";
-      var child = this.firstElementChild;
-
-      if (child && child.scrollWidth > child.offsetWidth) {
-        this.setAttribute(ATTR, this.getAttribute("data"));
-      } else {
-        this.removeAttribute(ATTR);
-      }
-    };
-
-    defineProperties(OptionalTooltip, [
-      {
-        key: "observedAttributes",
-        get: function get() {
-          return ["data"];
+    var OptionalTooltip = (function(parent) {
+      var upgrade = function() {
+        try {
+          return Reflect.construct(parent, arguments, this.constructor)
+        } catch (e) {
+          return parent.apply(this, arguments) || this
         }
       }
-    ]);
 
-    return OptionalTooltip;
-  })(HTMLElement);
+      function constructor() {}
 
-  if (window.customElements) {
-    customElements.define("optional-tooltip", OptionalTooltip);
+      function OptionalTooltip() {
+        var self = upgrade.apply(this, arguments)
+        return constructor.apply(self, arguments) || self
+      }
+
+      OptionalTooltip.prototype = Object.create(parent.prototype, {
+        constructor: {
+          configurable: true,
+          writable: true,
+          value: OptionalTooltip
+        }
+      })
+
+      OptionalTooltip.prototype.attributeChangedCallback = function attributeChangedCallback(
+        attributeName,
+        oldValue,
+        newValue,
+        namespace
+      ) {
+        var th = this
+
+        setTimeout(function() {
+          th.adjustTooltip()
+        }, 250)
+      }
+
+      OptionalTooltip.prototype.adjustTooltip = function adjustTooltip() {
+        var ATTR = 'data-tooltip'
+        var child = this.firstElementChild
+
+        if (child && child.scrollWidth > child.offsetWidth) {
+          this.setAttribute(ATTR, this.getAttribute('data'))
+        } else {
+          this.removeAttribute(ATTR)
+        }
+      }
+
+      defineProperties(OptionalTooltip, [
+        {
+          key: 'observedAttributes',
+          get: function get() {
+            return ['data']
+          }
+        }
+      ])
+
+      return OptionalTooltip
+    })(HTMLElement)
+
+    customElements.define('optional-tooltip', OptionalTooltip)
+  } catch (e) {
+    console.error(e)
   }
-} catch (e) {
-  console.error(e);
 }
 
-// TODO
-// Load polyfill () if customElements does not exist
-// - https://github.com/webcomponents/custom-elements
-// - https://cdn.rawgit.com/webcomponents/custom-elements/master/custom-elements.min.js
+if (window.customElements) {
+  init()
+} else {
+  var script = document.createElement('script')
+  script.src = 'https://unpkg.com/@webcomponents/custom-elements/custom-elements.min.js'
+  script.type = 'text/javascript'
+  script.onload = init
+  document.head.appendChild(script)
+}
+
+var data = { foo: 'bar' }
+
+<optional-tooltip class=`tooltip--top` data=`${data}`>
+</optional-tooltip>
+
+attrs.class === "tooltip-top"
